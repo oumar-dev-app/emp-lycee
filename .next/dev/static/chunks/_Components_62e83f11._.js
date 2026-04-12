fa0fc74c2396f921d6e3120554c3a44ff25711fb
oumar-dev-app <bourema1@icloud.com>
2026-04-12 17:09:28 +0000

    Ajout des nouveau composant

--- .next/dev/static/chunks/_Components_62e83f11._.js
+++ .next/dev/static/chunks/_Components_62e83f11._.js
@@ -55,7 +55,7 @@ function AccueilPremierSlider() {
         }
     }["AccueilPremierSlider.useEffect"], []);
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: " py-10",
+        className: " py-20",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
             className: "max-w-7xl m-auto    ",
             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
--- _Components/accueilPremierSlider.tsx
+++ _Components/accueilPremierSlider.tsx
@@ -38,7 +38,7 @@ function AccueilPremierSlider() {
   }, []);
 
   return (
-    <div className=" py-10">
+    <div className=" py-20">
       <div className="max-w-7xl m-auto    ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 shadow-2xl m-3 bg-gray-700 rounded-2xl">
 
