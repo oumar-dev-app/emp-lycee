bd2f620428f4469bb40cf6471b0fc149da2cc38b
oumar-dev-app <bourema1@icloud.com>
2026-04-13 17:03:59 +0000

    Modification au niveau de text

--- _Components/DifferentCentre.tsx
+++ _Components/DifferentCentre.tsx
@@ -32,10 +32,6 @@ function DifferentCentre() {
           <h1 className='text-2xl font-bold'>Chiffres clés</h1>
           <p className='flex items-center gap-4'>
             <FaCheck className="text-yellow-500 shrink-0" />
-             Effectif: 
-          </p>
-          <p className='flex items-center gap-4'>
-            <FaCheck className="text-yellow-500 shrink-0" />
             Periode d'activité : depuis Octobre 2013
           </p>
         </div>
